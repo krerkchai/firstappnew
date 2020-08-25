4c2d98bac064fcd63466be41b1b10f8f6593ede2
USER <ppopp9450@gmail.com>
2020-08-25 15:45:14 +0700

    add MainActivity.kt

--- app/src/main/java/cs/rmuti/example/mobile/firstappnew/MainActivity.kt
+++ app/src/main/java/cs/rmuti/example/mobile/firstappnew/MainActivity.kt
@@ -2,15 +2,17 @@ package cs.rmuti.example.mobile.firstappnew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import cs.rmuti.example.mobile.firstappnew.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding :ActivityMainBinding
     private lateinit var drawer_layout:DrawerLayout
@@ -28,9 +30,13 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+
     }
 
-    fun onSupportNavigateUp(item:MenuItem): Boolean {
+    //override fun onSupportNavigateUp(item:MenuItem): Boolean {
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.nav_aboutme -> {
                 supportFragmentManager.beginTransaction().replace(R.id.fragment_contain,AboutmeFragment()).commit()
@@ -40,8 +46,9 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.nav_list -> {
                 supportFragmentManager.beginTransaction().replace(R.id.fragment_contain,AddListFragment()).commit()
+            }
         }
-        }
+        drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
-}
+    }
