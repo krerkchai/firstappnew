96231283399d748f3fc310d0149818350384979b
USER <ppopp9450@gmail.com>
2020-08-25 12:35:34 +0700

    MainActivity.kt

--- app/src/main/java/cs/rmuti/example/mobile/firstappnew/MainActivity.kt
+++ app/src/main/java/cs/rmuti/example/mobile/firstappnew/MainActivity.kt
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import cs.rmuti.example.mobile.firstappnew.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding :ActivityMainBinding
     private lateinit var drawer_layout:DrawerLayout
+    private lateinit var navgation_view: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        drawer_layout = binding.drawerlayout
+        navgation_view = binding.navView
 
     }
 }
