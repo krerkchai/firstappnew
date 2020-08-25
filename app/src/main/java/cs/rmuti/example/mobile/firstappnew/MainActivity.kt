6e1e33341234b9c790b3557b3c6b6c560594fa09
USER <ppopp9450@gmail.com>
2020-08-25 13:32:03 +0700

    strings.xml

--- app/src/main/java/cs/rmuti/example/mobile/firstappnew/MainActivity.kt
+++ app/src/main/java/cs/rmuti/example/mobile/firstappnew/MainActivity.kt
@@ -2,6 +2,8 @@ package cs.rmuti.example.mobile.firstappnew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding :ActivityMainBinding
     private lateinit var drawer_layout:DrawerLayout
     private lateinit var navgation_view: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,20 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         drawer_layout = binding.drawerlayout
         navgation_view = binding.navView
+        toggle = ActionBarDrawerToggle (this,drawer_layout,R.string.open,R.string.close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+    }
+
+    fun onSupportNavigateUp(item:MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_aboutme -> {
+                supportFragmentManager.beginTransaction().replace(R.id.)
+
+            }
+        }
+        return true
     }
 }
