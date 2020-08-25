ff7658785451429719a67ccdc487c3c0692fe067
USER <ppopp9450@gmail.com>
2020-08-25 12:16:09 +0700

    activity_main.xml build.gradle

--- app/src/main/java/cs/rmuti/example/mobile/firstappnew/MainActivity.kt
+++ app/src/main/java/cs/rmuti/example/mobile/firstappnew/MainActivity.kt
@@ -2,10 +2,18 @@ package cs.rmuti.example.mobile.firstappnew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var drawer_layout:DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
